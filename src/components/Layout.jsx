a5bf605f88fdeb9543b71bbcecc94193df3ca547
AlanPlaun <xeneisesbosteros@gmail.com>
2023-11-26 13:35:52 -0300

    tp8 de Alan

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,45 +1,20 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import { ContextUsuario } from "../context/ContextUsuario";
 
 
 const Layout = () => {
-    const {autenticado} = React.useContext(ContextUsuario);
-    if (autenticado === true)
-    {
-        return(
-            <React.Fragment>
-            <div>
-                <nav className='navbar'>
-                    <Link to={'/perfil'} className='nav-link'><img src="/perfil.png" width={50} alt='Perfil'/></Link>
-                    <Link to={'/'} className='nav-link'><h3>Home</h3></Link>
-                    <Link to={'/informacion'} className='nav-link'><h3>+Info</h3></Link>
-                </nav>
-            </div>
-            <div className='content'>
-                <Outlet/>
-            </div>
-        </React.Fragment>
-        )
-    }
-            
-    else {
-        return (
-        <React.Fragment>
-            <div>
-                <nav className='navbar'>
-                    <Link to={'/login'} className='nav-link'><img src="/perfil.png" width={50} alt='Login'/></Link>
-                    <Link to={'/'} className='nav-link'><h3>Home</h3></Link>
-                    <Link to={'/informacion'} className='nav-link'><h3>+Info</h3></Link>
-                </nav>
-            </div>
-            <div className='content'>
-                <Outlet/>
-            </div>
-        </React.Fragment>
-        )
-    }
-
+    return (
+    <React.Fragment>
+        <div>
+            <nav className='navbar'>
+                <Link to={'/'} className='nav-link'>Home</Link>
+                <Link to={'/informacion'} className='nav-link'>Informacion</Link>
+            </nav>
+        </div>
+        <div className='content'>
+            <Outlet/>
+        </div>
+    </React.Fragment>)
 }
 
 export default Layout
