828d5c4d166769df34660b93ece1d70b3d2096b6
ALAN PLAUN <47165921@ort>
2023-11-27 08:05:36 -0300

    terminado 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Carrousel from './Components/Carrousel';
-import Layout from './Components/Layout';
-import Home from './Components/Home';
-import Informacion from './Components/Informacion';
-import Sesion from './Components/Sesion';
-import Perfil from './Components/Perfil';
+import Carrousel from './components/Carrousel';
+import Layout from './components/Layout'
+import Home from './components/Home';
+import Informacion from './components/Informacion';
+import Sesion from './components/Sesion';
+import Perfil from './components/Perfil';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Asegúrate de importar los componentes necesarios para el enrutamiento
-import SesionProvider from './Components/SesionContext';
+import SesionProvider from './components/SesionContext';
 
 function App() {
   return (
